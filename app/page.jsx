31482c7c572751a7475fd68e56e155f7992552ae
diegonicita <diegonicita@gmail.com>
2024-09-27 17:49:56 -0300

    First commit

---
+++ app/page.jsx
@@ -0,0 +1,84 @@
+'use client'
+
+import dynamic from 'next/dynamic'
+import { Suspense } from 'react'
+
+const Logo = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Logo), { ssr: false })
+const Dog = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Dog), { ssr: false })
+const Duck = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Duck), { ssr: false })
+const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.View), {
+  ssr: false,
+  loading: () => (
+    <div className='flex h-96 w-full flex-col items-center justify-center'>
+      <svg className='-ml-1 mr-3 size-5 animate-spin text-black' fill='none' viewBox='0 0 24 24'>
+        <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4' />
+        <path
+          className='opacity-75'
+          fill='currentColor'
+          d='M4 12a8 8 0 0 1 8-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0 1 4 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+        />
+      </svg>
+    </div>
+  ),
+})
+const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mod.Common), { ssr: false })
+
+export default function Page() {
+  return (
+    <>
+      <div className='mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5'>
+        {/* jumbo */}
+        <div className='flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left'>
+          <p className='w-full uppercase'>Bienvenido a YourSite</p>
+          <h1 className='my-4 text-5xl font-bold leading-tight'>RTF Example</h1>
+          <p className='mb-8 text-2xl leading-normal'>
+            Un proyecto minimalista para comenzar con React, React-three-fiber y Threejs.
+          </p>
+        </div>
+
+        <div className='w-full text-center md:w-3/5'>
+          <View className='flex h-96 w-full flex-col items-center justify-center'>
+            <Suspense fallback={null}>
+              <Logo route='/blob' scale={0.6} position={[0, 0, 0]} />
+              <Common />
+            </Suspense>
+          </View>
+        </div>
+      </div>
+
+      <div className='mx-auto flex w-full flex-col flex-wrap items-center p-12 md:flex-row  lg:w-4/5'>
+        {/* first row */}
+        <div className='relative h-48 w-full py-6 sm:w-1/2 md:my-12 md:mb-40'>
+          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Los Eventos se propagan</h2>
+          <p className='mb-8 text-gray-600'>Arrastra, rota o haz scroll en el canvas para explorar la escena 3D.</p>
+        </div>
+        <div className='relative my-12 h-48 w-full py-6 sm:w-1/2 md:mb-40'>
+          <View orbit className='relative h-full  sm:h-48 sm:w-full'>
+            <Suspense fallback={null}>
+              <Dog scale={2} position={[0, -1.6, 0]} rotation={[0.0, -0.3, 0]} />
+              <Common color={'lightpink'} />
+            </Suspense>
+          </View>
+        </div>
+        {/* second row */}
+        <div className='relative my-12 h-48 w-full py-6 sm:w-1/2 md:mb-40'>
+          <View orbit className='relative h-full animate-bounce sm:h-48 sm:w-full'>
+            <Suspense fallback={null}>
+              <Duck route='/blob' scale={2} position={[0, -1.6, 0]} />
+              <Common color={'lightblue'} />
+            </Suspense>
+          </View>
+        </div>
+        <div className='w-full p-6 sm:w-1/2'>
+          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>El Dom y lo 3D estan sincronizados</h2>
+          <p className='mb-8 text-gray-600'>
+            Los divs 3D se renderizan a través del componente View. Utiliza gl.scissor para dividir el viewport en
+            segmentos. Asocias una vista a un div de seguimiento, que luego controla la posición y los límites del
+            viewport. Esto te permite tener múltiples vistas con un solo canvas de alto rendimiento. Estas vistas
+            seguirán a sus elementos de seguimiento, se desplazarán, redimensionarán, etc.
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
